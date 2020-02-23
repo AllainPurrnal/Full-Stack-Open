94f8d2aabbc56f636937506a08d135a8395ba3e7
AllainPurrnal <llnbrnl017@gmail.com>
2020-02-22 23:39:51 -0800

    Unicafe Step 3 completed

--- Part 1/unicafe/src/index.js
+++ Part 1/unicafe/src/index.js
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Statistics = (props) => {
+  return (
+    <>
+      {console.log(props)}
+      <p> Good: {props.goodVal} </p>
+      <p> Neutral: {props.neutralVal} </p>
+      <p> Bad: {props.badVal} </p>
+    </>
+  )
+}
+
 const App = () => {
   const [ good, setGood ] = useState(0)
   const [ neutral, setNeutral ] = useState(0)
@@ -8,30 +19,28 @@ const App = () => {
 
   const goodFeedback = (newFeedback) => {
     setGood(newFeedback)
-    console.log("Good:", newFeedback)
+    // console.log("Good:", newFeedback)
   }
 
   const neutralFeedback = (newFeedback) => {
     setNeutral(newFeedback)
-    console.log("Neutral:", newFeedback)
+    // console.log("Neutral:", newFeedback)
   }
 
   const badFeedback = (newFeedback) => {
     setBad(newFeedback)
-    console.log("Bad:", newFeedback)
+    // console.log("Bad:", newFeedback)
   }
 
   return (
     <div>
       <h1> Feedback! </h1>
-      <button onClick={() => goodFeedback(good + 1)} > Good </button>
-      <button onClick={() => neutralFeedback(neutral + 1)} > Neutral </button>
-      <button onClick={() => badFeedback(bad + 1)}> Bad </button>
+        <button onClick={() => goodFeedback(good + 1)} > Good </button>
+        <button onClick={() => neutralFeedback(neutral + 1)} > Neutral </button>
+        <button onClick={() => badFeedback(bad + 1)}> Bad </button>
 
-      <h3>Stats</h3>
-      <p> Good: {good} </p>
-      <p> Neutral: {neutral} </p>
-      <p> Bad: {bad} </p>
+      <h3> Stats </h3>
+      <Statistics goodVal={good} neutralVal={neutral} badVal={bad}/>
     </div>
   )
 }
