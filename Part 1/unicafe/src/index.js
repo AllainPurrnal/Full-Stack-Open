17ee82f275c8ec732b5eede303e2ae91baab1a81
Allain Bernal <llnbrnl017@gmail.com>
2020-02-23 19:16:39 -0800

    Completed Step 4 of Unicafe Exercise

--- Part 1/unicafe/src/index.js
+++ Part 1/unicafe/src/index.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Statistics = (props) => {
+  if (props.feedbackVal === 0) {
+    return (
+      <div>
+        <p>No feedback has been received 😭</p>
+      </div>
+    )
+  }
+  
   return (
     <>
       {console.log(props)}
       <p> Good: {props.goodVal} </p>
       <p> Neutral: {props.neutralVal} </p>
       <p> Bad: {props.badVal} </p>
+      <p> All: {props.feedbackVal} </p>
+      <p> Positive: {props.goodVal / props.feedbackVal} </p>
     </>
   )
 }
@@ -16,31 +26,35 @@ const App = () => {
   const [ good, setGood ] = useState(0)
   const [ neutral, setNeutral ] = useState(0)
   const [ bad, setBad ] = useState(0)
+  const [ feedback, setFeedback ] = useState(0)
 
-  const goodFeedback = (newFeedback) => {
-    setGood(newFeedback)
+  const goodFeedback = (good) => {
+    setGood(good + 1)
+    setFeedback(feedback + 1)
     // console.log("Good:", newFeedback)
   }
 
-  const neutralFeedback = (newFeedback) => {
-    setNeutral(newFeedback)
+  const neutralFeedback = (neutral) => {
+    setNeutral(neutral + 1)
+    setFeedback(feedback + 1)
     // console.log("Neutral:", newFeedback)
   }
 
-  const badFeedback = (newFeedback) => {
-    setBad(newFeedback)
+  const badFeedback = (bad) => {
+    setBad(bad + 1)
+    setFeedback(feedback + 1)
     // console.log("Bad:", newFeedback)
   }
 
   return (
     <div>
       <h1> Feedback! </h1>
-        <button onClick={() => goodFeedback(good + 1)} > Good </button>
-        <button onClick={() => neutralFeedback(neutral + 1)} > Neutral </button>
-        <button onClick={() => badFeedback(bad + 1)}> Bad </button>
+        <button onClick={() => goodFeedback(good)} > Good </button>
+        <button onClick={() => neutralFeedback(neutral)} > Neutral </button>
+        <button onClick={() => badFeedback(bad)}> Bad </button>
 
       <h3> Stats </h3>
-      <Statistics goodVal={good} neutralVal={neutral} badVal={bad}/>
+      <Statistics goodVal={good} neutralVal={neutral} badVal={bad} feedbackVal={feedback} />
     </div>
   )
 }
