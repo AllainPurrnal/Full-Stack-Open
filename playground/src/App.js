c503e57b54e10c11049e8681aa6bb2e940e80917
Allain Bernal <llnbrnl017@gmail.com>
2020-03-29 21:26:22 -0700

    Pausing work on FullStackOpen. Need to refresh on core js

--- playground/src/App.js
+++ playground/src/App.js
@@ -4,11 +4,13 @@ const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '111-111-1111', id: 1 },
     { name: 'Zark Muckerberg', number: '222-222-2222', id: 2 },
-    { name: 'Elon Tusk', number: '333-333-3333', id: 3 }
+    { name: 'Mon Elusk', number: '333-333-3333', id: 3 },
+    { name: 'Jeve Stobs', number: '444-444-4444', id: 4}
   ])
 
   const [ newName, setNewName ] = useState('')
   const [ newNum, setNewNum ] = useState('')
+  const [ newSearch, setNewSearch ] = useState('')
 
   const addContact = (event) => {
     event.preventDefault();
@@ -33,7 +35,22 @@ const App = () => {
     setNewNum('')
   }
 
-  
+  const filterView = () => {
+    if (newSearch === '') {
+      return persons.map((person, id) => 
+        <p key={person.id}>{person.name}: {person.number}</p>
+      )
+    }
+
+    return console.log(
+      [...persons].filter(
+        person => person.name.toLowerCase().includes(newSearch.toLowerCase())
+      )
+    )
+    
+
+  }
+
   const handleNameChange = (event) => {
     // console.log(event.target.value)
     setNewName(event.target.value)
@@ -42,20 +59,31 @@ const App = () => {
     // console.log(event.target.value)
     setNewNum(event.target.value)
   }
+  const handleSearchChange = (event) => {
+    // console.log(event.target.value)
+    setNewSearch(event.target.value)
+  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addContact}>
-        <div>name: <input value={newName} onChange={handleNameChange} /></div>
-        <div>phone: <input value={newNum} onChange={handleNumChange} /></div>
-        <div><button type="submit">add</button></div>
-      </form>
+      <h1>Phonebook</h1>
+
+      <div>
+        <h2>Look up</h2>
+        <input value={newSearch} onChange={handleSearchChange}/>
+      </div>
+
+      <div>
+        <h2>Add Contact</h2>
+        <form onSubmit={addContact}>
+          <div>name: <input value={newName} onChange={handleNameChange} /></div>
+          <div>phone: <input value={newNum} onChange={handleNumChange} /></div>
+          <div><button type="submit">add</button></div>
+        </form>
+      </div>
 
       <h2>Numbers</h2>
-      {persons.map((person, id) =>
-        <p key={person.id}>{person.name}: {person.number}</p>
-      )}
+      {filterView()}
 
     </div>
   )
